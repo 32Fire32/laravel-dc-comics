4b60fd6245e614993d5e57fecd6b178a1afc3c15
32Fire32 <nicola.gianfelici@hotmail.it>
2023-01-19 16:37:40 +0100

    stampa nella index tutti i comics del file comics.php

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -8,7 +8,6 @@
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Titolo</th>
-                    <th scope="col">Descrizione</th>
                     <th scope="col">Url immagine</th>
                     <th scope="col">Prezzo</th>
                     <th scope="col">Serie</th>
@@ -18,19 +17,19 @@
                 </tr>
             </thead>
             <tbody>
-                {{-- @foreach ($comics as $comic)
+                @foreach ($comics as $comic)
                     <tr>
                         <td>{{ $comic->id }}</td>
                         <td>{{ $comic->title }}</td>
-                        <td>{{ $comic->description }}</td>
                         <td>{{ $comic->thumb }}</td>
                         <td>{{ $comic->price }}</td>
                         <td>{{ $comic->series }}</td>
                         <td>{{ $comic->sale_date }}</td>
                         <td>{{ $comic->type }}</td>
-                        <td>{{ ... }}</td>a
+                        <td><a href="{{ route('comics.show', $comic->id) }}"class="btn btn-primary">Insert new comic</a>
+                        </td>
                     </tr>
-                @endforeach --}}
+                @endforeach
 
             </tbody>
         </table>
