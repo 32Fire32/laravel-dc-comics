845b9b1ffb28d06e130416047e9b43023caa436f
32Fire32 <nicola.gianfelici@hotmail.it>
2023-01-23 16:24:35 +0100

    corregge bug sulla validazione del prezzo in fase di creazione

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -41,7 +41,7 @@ class ComicController extends Controller
             'title'=>'required|string|min:1|max:50',
             'description' => 'nullable|string',
             'thumb' => 'nullable',
-            'price' => 'required|numeric',
+            'price' => 'required|numeric|max:1000',
             'series' => 'required|string|max:100',
             'sale_date' => 'required|date',
             'type' => 'required|string|max:50'
